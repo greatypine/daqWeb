77d6085d77a0710c8c735b91fdf63e961cf12cf7
greatypine <greatypine@163.com>
2018-05-10 21:21:23 +0800

    add city js

--- WebContent/demo/js/wulanchabu.js
+++ WebContent/demo/js/wulanchabu.js
@@ -1,9 +1,15 @@
-var storeServiceDataOfCity = function(cityname){
+var wulanchabu_storeServiceDataOfCity = function(){
 	var serviceData = {"code":200,"data":[{"storeNo":"0474E0001","polygonMap":"","vertex":[[113.172005,40.977462],[113.185738,40.977981],[113.185523,40.969783],[113.171919,40.96907],[113.171876,40.972764],[113.153337,40.973088],[113.153634,40.968324],[113.148055,40.968583],[113.148484,40.981252],[113.16256,40.981155],[113.162517,40.9842],[113.172044,40.984168],[113.172002,40.977461]],"areaName":"乌兰察布店","acreage":3382281,"storeName":"乌兰察布职业学院店","_id":{"$oid":"58575526e4b0d6bddbe822db"},"_class":"com.guoanshequ.modules.guoan.entity.StoreArea","position":[113.17664,40.97534],"storeId":"8ad8958757055ba801573c93b2be3b8d","status":0}],"message":"请求成功"}
 	return serviceData;
 }
 
-var storeCoordinatesDataOfCity = function(cityname){
+var wulanchabu_storeCoordinatesDataOfCity = function(){
 	var coordinates={"code":200,"data":[],"message":"请求成功"}
 	return coordinates;
+}
+
+var wulanchabu_pianqu = function(){
+//乌兰察布职业学院店
+	var pianquxinxi = {"coordinate":[],"code":200,"serviceArea":[[113.172005,40.977462],[113.185738,40.977981],[113.185523,40.969783],[113.171919,40.96907],[113.171876,40.972764],[113.153337,40.973088],[113.153634,40.968324],[113.148055,40.968583],[113.148484,40.981252],[113.16256,40.981155],[113.162517,40.9842],[113.172044,40.984168],[113.172002,40.977461]],"areas":[],"store":{"mobilephone":null,"store_id":142,"town_id":"1343","nature":null,"update_time":1490335953000,"create_time":null,"remark":null,"update_user":"刘孟雨","create_user":null,"sort":null,"house_id":null,"county_id":115,"town_name":"白海子镇","province_id":5,"city_id":35,"cityName":"乌兰察布","village_id":null,"storeno":"0474E0001","caozuo":null,"building_id":null,"formattype":null,"place_town_id":null,"store_position":null,"work_id":null,"auditor_status":3,"county_ids":"115","tenancyTerm":null,"rental":null,"payment_method":null,"rent_area":null,"usable_area":null,"increase":null,"rent_free":null,"taxes":null,"agency_fee":null,"increase_fee":null,"storetype":"E","storetypename":"校园店","open_shop_time":null,"cityNo":"0474","detail_address":null,"superMicro":null,"estate":null,"ordnumber":1,"platformname":"乌兰察布职业学院店","white":"normal","platformid":"8ad8958757055ba801573c93b2be3b8d","tinyvillage_Id":null,"coordinate_x":null,"coordinate_y":null,"open_time":null,"skid":110873,"rmid":null,"gaode_cityCode":null,"gaode_provinceCode":null,"gaode_adCode":null,"gaode_address":null,"flag":0,"status":0,"address":"内蒙古自治区乌兰察布市察哈尔右翼前旗乌兰察布职业学院","name":"乌兰察布职业学院店","id":"8ad8958757055ba801573c93b2be3b8d","type":1,"number":139,"version":275},"message":"请求成功"} 
+	return pianquxinxi;
 }
