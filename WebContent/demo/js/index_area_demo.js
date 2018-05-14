64951f778c312ae54a46def3fda5cfb3bab46fe8
gaobaolei <2636130684@qq.com>
2018-05-14 11:06:07 +0800

    demo展示数据调整

--- WebContent/demo/js/index_area_demo.js
+++ WebContent/demo/js/index_area_demo.js
@@ -1,15 +1,16 @@
 var STORE_DAILY_TOTAL_GMV= 33467;
-var AREA_ORDER_AMOUNT = Math.round((1.1-Math.random())*300);
+var AREA_ORDER_AMOUNT = 482;
 var AREA_HOUSE_AMOUNT = 502;
+var AREA_ORDER  = {"0022Y003000018":286,"0022Y003000019":312,"0022Y003000020":301,"0022Y003000021":290,"0022Y003000022":434,"0022Y003000023":376,"0022Y003000026":517,"0022Y003000027":289,"0022Y003000029":476,"0022Y003000034":297};
 
-
+var seven_day_gmv = {"0022Y003000018":[24126,18728.6,24625.5,24697,26092,23623,28097],"0022Y003000019":[12126,22728.6,19625.5,21097,26092,23623,28097],"0022Y003000020":[13226,12718,15525.5,23097,26092,25623,23997],"0022Y003000021":[12226,26928.6,13925.5,20997,26992,28567,25197],"0022Y003000022":[18226,27928.6,16925.5,22997,27992,26567,31197],"0022Y003000023":[18126,27728.6,16625.5,24097,28092,22823,28097],"0022Y003000026":[20126,28728.6,19654,28097,30092,28723,28097],"0022Y003000027":[12126,28728.6,14625.5,21097,27092,23623,29097],"0022Y003000029":[19226,21928.6,19925.5,24977,27992,26567,29197],"0022Y003000034":[12126,27728.6,14625.5,21097,26092,23623,28097]};
 //获取近期日gmv
 function getRecentlySevenDay_gmv(){
     var key=[];
-	var value = [];
+	
 	var curDate = new Date();
     
-	for(var i=0;i<8;i++){
+	for(var i=0;i<7;i++){
 		var preDate = new Date(curDate.getTime() - i*24*60*60*1000); //前i天
 		var c_year = preDate.getFullYear(); 
 		var c_month = preDate.getMonth()+1;
@@ -22,23 +23,25 @@ function getRecentlySevenDay_gmv(){
 		}
 		var dateStr = c_year+"-"+c_month+"-"+c_day;
 		key.push(dateStr);
-		value.push(((1.1-Math.random())*10000).toFixed(2));
+		//value.push(((1.1-Math.random())*10000).toFixed(2));
 	}
 	
-	var kv = {"xAxis":key,"series":value};
+	var kv = {"xAxis":key.reverse(),"series":seven_day_gmv[areaNo]};
 	return kv;
 	
 }
 
 
 //获取近期日用户量(拉新，消费)
+var area_laxin={"0022Y003000018":[20,19,37,39,42,24,56],"0022Y003000019":[18,22,35,40,30,29,49],"0022Y003000020":[19,24,32,41,42,31,49],"0022Y003000021":[20,25,31,39,10,29,30],"0022Y003000022":[21,22,31,39,20,29,51],"0022Y003000023":[15,30,31,40,21,26,50],"0022Y003000026":[15,30,32,40,24,26,59],"0022Y003000027":[16,32,36,41,24,26,52],"0022Y003000029":[16,32,34,41,27,28,52],"0022Y003000034":[13,32,39,41,27,36,59]};
+var area_consumer = {"0022Y003000018":[21,21,32,26,41,21,40],"0022Y003000019":[11,22,30,42,41,29,41],"0022Y003000020":[20,21,31,48,42,31,70],"0022Y003000021":[20,21,31,38,24,27,58],"0022Y003000022":[22,29,32,36,25,28,40],"0022Y003000023":[16,40,32,46,24,27,59],"0022Y003000026":[12,40,34,48,26,29,51],"0022Y003000027":[12,32,37,42,28,29,53],"0022Y003000029":[12,25,39,41,28,40,55],"0022Y003000034":[14,30,36,42,28,38,30]};
 function getRecentlySevenDay_customer(){
     var key=[];
-	var value0 = [];
-	var value1 = [];
+	//var value0 = [19,23,20,38,41,28,56];
+	//var value1 = [22,40,21,42,50,43,60];
 	var curDate = new Date();
     
-	for(var i=0;i<8;i++){
+	for(var i=0;i<7;i++){
 		var preDate = new Date(curDate.getTime() - i*24*60*60*1000); //前i天
 		var c_year = preDate.getFullYear(); 
 		var c_month = preDate.getMonth()+1;
@@ -51,25 +54,26 @@ function getRecentlySevenDay_customer(){
 		}
 		var dateStr = c_year+"-"+c_month+"-"+c_day;
 		key.push(dateStr);
-		var laxin = Math.round((1.1-Math.random())*50);
-		value0.push(laxin);
-		value1.push(laxin+Math.round((1.1-Math.random())*50));
+		//var laxin = Math.round((1.1-Math.random())*50);
+		//value0.push(laxin);
+		//value1.push(laxin+Math.round((1.1-Math.random())*50));
 	}
 	
-	var kv = {"xAxis":key,"series0":value0,"series1":value1};
+	var kv = {"xAxis":key.reverse(),"series0":area_laxin[areaNo],"series1":area_consumer[areaNo]};
 	return kv;
 	
 }
 
 //获取事业群当月的gmv
 var deptArray = ['家务事','老年之家','国安优易','公共','商业创新','旅游','健康'];
+var dept_gmv = [7119.95,10476.86,7895.21,3312.39,1426.80,3999.32,8586.79];
 function getDepartment_gmv(){
     var deptNV = new Array();
 	
 	for(var i=0;i<deptArray.length;i++){
 		var vnMap = {};
-		var v = ((1.1-Math.random())*10000).toFixed(2);
-		vnMap["value"] = v;
+		//var v = ((1.1-Math.random())*10000).toFixed(2);
+		vnMap["value"] = dept_gmv[i];
 		vnMap["name"] = deptArray[i];
 		deptNV.push(vnMap);
 	}
@@ -79,13 +83,14 @@ function getDepartment_gmv(){
 
 //获取频道当月gmv
 var channelArray = ['家修','养老餐','日用百货','社区书屋','休闲食品','安心出游','医药服务'];
+var channel_gmv = [1122.55,5571.26,9682.89,2544.96,5470.97,6843.34,6868.95];
 function getChannel_gmv(){
     var channelNV = new Array(); 
 	
 	for(var i=0;i<channelArray.length;i++){
 		var vnMap = {};
-		var v = ((1.1-Math.random())*10000).toFixed(2);
-		vnMap["value"] = v;
+		//var v = ((1.1-Math.random())*10000).toFixed(2);
+		vnMap["value"] = channel_gmv[i];
 		vnMap["name"] = channelArray[i];
 		channelNV.push(vnMap);
 	}
@@ -95,13 +100,14 @@ function getChannel_gmv(){
 
 
 //获取事业群当月的消费用户
+var dept_consumer = [86,52,106,10,28,89,18];
 function getDepartment_consumer(){
     var deptNV = new Array();
 	
 	for(var i=0;i<deptArray.length;i++){
 		var vnMap = {};
-		var v = Math.round((1.1-Math.random())*100);
-		vnMap["value"] = v;
+		//var v = Math.round((1.1-Math.random())*100);
+		vnMap["value"] = dept_consumer[i];
 		vnMap["name"] = deptArray[i];
 		deptNV.push(vnMap);
 	}
@@ -110,13 +116,14 @@ function getDepartment_consumer(){
 }
 
 //获取频道当月消费用户
+var channel_consumer = [50,32,76,4,20,59,13];
 function getChannel_consumer(){
     var channelNV = new Array(); 
 	
 	for(var i=0;i<channelArray.length;i++){
 		var vnMap = {};
-		var v = Math.round((1.1-Math.random())*60);
-		vnMap["value"] = v;
+		//var v = Math.round((1.1-Math.random())*60);
+		vnMap["value"] = channel_consumer[i];
 		vnMap["name"] = channelArray[i];
 		channelNV.push(vnMap);
 	}
