2a8462af53ee4e77a4bcda9e9ddd5965b20fdb16
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-05-22 15:36:35 +0800

    添加同步门店经度纬度字段

--- src/com/cnpc/pms/dynamic/manager/DynamicManager.java
+++ src/com/cnpc/pms/dynamic/manager/DynamicManager.java
@@ -801,7 +801,7 @@ public interface DynamicManager extends IManager{
   	public Map<String, Object> exportData(DynamicDto dd,String str);
   	
     public JSONObject insertNewTest(String storeCode,String storeName);
-    public JSONObject insertNewStore(String storeCode,String storeName,String provinceCode,String cityCode,String adCode,String address);
+    public JSONObject insertNewStore(String storeCode,String storeName,String provinceCode,String cityCode,String adCode,String address,String longitude,String latitude);
     public JSONObject insertNewEmployee(String storeCode,String employeeCode,String employeeName,String telephone);
 
     
