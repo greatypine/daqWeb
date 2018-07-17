b75fad8b5a9a1fc9ff0844175ca96a00f78286ef
chenchuang <chenchuang@guoanshequ.com>
2018-07-17 09:19:23 +0800

    221页面改版

--- src/com/cnpc/pms/dynamic/dao/impl/StrategyActivityDaoImpl.java
+++ src/com/cnpc/pms/dynamic/dao/impl/StrategyActivityDaoImpl.java
@@ -14,23 +14,20 @@ import com.cnpc.pms.dynamic.dao.StrategyActivityDao;
 public class StrategyActivityDaoImpl extends BaseDAOHibernate implements StrategyActivityDao {
 
 	@Override
-	public Map<String, Object> queryStrategyGMV(String dept_id,String start_time,String end_time) {
+	public Map<String, Object> queryStrategyGMV(String dept_id,String store_no,String start_time,String end_time) {
 		String sql = "SELECT IFNULL(SUM(trading_price),0) total_gmv FROM df_mass_order_monthly tor "
-				+ "JOIN df_activity_scope das ON (tor.store_id = das.platformid) WHERE 1=1 ";
-		
+				+ "JOIN df_activity_scope das ON (tor.store_id = das.platformid) WHERE tor.order_tag2 IS NOT NULL  ";
+		if(StringUtils.isNotEmpty(dept_id)){
+			sql  = sql + " AND tor.order_tag2='"+dept_id+"' ";
+		}
+		if(StringUtils.isNotEmpty(store_no)){
+			sql = sql + " AND tor.store_code = '"+store_no+"' ";
+		}
 		if(StringUtils.isNotEmpty(start_time)){
 			sql = sql + " AND date(tor.sign_time) >= '"+start_time+"' ";
 		}
 		if(StringUtils.isNotEmpty(end_time)){
 			sql = sql + " AND date(tor.sign_time) <= '"+end_time+"' ";
-		}
-		
-		if(StringUtils.isNotEmpty(dept_id)){
-			if("groupon".equals(dept_id)){
-				sql  = sql + " AND tor.groupon_instance_id IS NOT NULL ";
-			}else if(!"all".equals(dept_id)){
-				sql  = sql + " AND tor.bussiness_group_id = '"+dept_id+"'";
-			}
 		}
 		
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
@@ -42,11 +39,20 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 		}
 		return order_obj;
 	}
-
+	
 	@Override
-	public List<Map<String, Object>> queryGmvTrend(){
-		String sql = "SELECT IFNULL(SUM(trading_price), 0) total_gmv,DATE(tor.sign_time) AS datetime FROM df_mass_order_monthly tor "
-				+ "JOIN df_activity_scope das ON (tor.store_id = das.platformid) WHERE DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(tor.sign_time) GROUP BY datetime";
+	public List<Map<String, Object>> queryUnStrategyGMV(String start_time,String end_time){
+		String sql = "SELECT IFNULL(SUM(trading_price), 0) total_gmv,department_name FROM df_mass_order_monthly tor WHERE tor.order_tag2 IS NULL ";
+		if(StringUtils.isNotEmpty(start_time)){
+			sql = sql + " AND date(tor.sign_time) >= '"+start_time+"' ";
+		}
+		if(StringUtils.isNotEmpty(end_time)){
+			sql = sql + " AND date(tor.sign_time) <= '"+end_time+"' ";
+		}
+		sql = sql + " AND tor.department_name IS NOT NULL AND tor.department_name NOT LIKE '测试%' "
+				//+ "AND tor.store_id NOT IN (SELECT platformid FROM df_activity_scope) "
+				+ "AND NOT EXISTS (SELECT platformid FROM df_activity_scope	WHERE platformid = tor.store_id)"
+				+ "GROUP BY bussiness_group_id";
 		
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
 		// 获得查询数据
@@ -55,9 +61,31 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 	}
 	
 	@Override
-	public Map<String, Object> queryNewMember() {
-		String sql = "SELECT count(member.customer_id) AS membernum FROM df_user_member member JOIN df_activity_scope das ON (member.regist_storeid = das.platformid) "
-				+ "WHERE member.opencard_time >= '2018-07-01' ";
+	public List<Map<String, Object>> queryGmvTrend(){
+		String sql = "SELECT IFNULL(SUM(trading_price), 0) total_gmv,DATE(tor.sign_time) AS datetime FROM df_mass_order_monthly tor "
+				+ "JOIN df_activity_scope das ON (tor.store_id = das.platformid) WHERE DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(tor.sign_time) "
+				+ "AND tor.order_tag2 IS NOT NULL GROUP BY datetime";
+		
+		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
+		// 获得查询数据
+		List<Map<String, Object>> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		return lst_data;
+	}
+	
+	@Override
+	public Map<String, Object> queryNewMember(String store_no,String start_time,String end_time) {
+		String sql = "SELECT count(member.customer_id) AS membernum FROM df_user_member member JOIN df_activity_scope das ON (member.regist_storeid = das.platformid ";
+		if(StringUtils.isNotEmpty(store_no)){
+			sql = sql + "AND das.store_no='"+store_no+"'";
+		}
+		sql = sql + ")WHERE 1=1 ";
+		
+		if(StringUtils.isNotEmpty(start_time)){
+			sql = sql + " AND date(member.opencard_time) >= '"+start_time+"' ";
+		}
+		if(StringUtils.isNotEmpty(end_time)){
+			sql = sql + " AND date(member.opencard_time) <= '"+end_time+"' ";
+		}
 				
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
 		// 获得查询数据
@@ -82,12 +110,33 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 		}
 		return order_obj;	
 	}
-
+	
 	@Override
-	public List<Map<String, Object>> queryDataOfScatterplot() {
-		String sql = "SELECT das.store_name,tor.ordergmv,tor.ordernum FROM(SELECT tor.store_id,sum(trading_price) AS ordergmv,count(1) AS ordernum "
-				+ "FROM df_mass_order_monthly tor FORCE INDEX(sign_time) WHERE TO_DAYS(NOW()) - TO_DAYS(tor.sign_time) <= 1 GROUP BY tor.store_id) tor "
-				+ "JOIN df_activity_scope das ON (tor.store_id = das.platformid)";
+	public Map<String, Object> queryStoreUser(String store_no){
+		String sql = "SELECT tu.`name` AS store_user FROM	t_store ts,tb_bizbase_user tu WHERE	ts.skid = tu.id AND ts.storeno = '"+store_no+"'";
+		
+		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
+		// 获得查询数据
+		Map<String, Object> order_obj = null;
+		List<?> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		if (lst_data != null && lst_data.size() > 0) {
+			order_obj = (Map<String, Object>) lst_data.get(0);
+		}
+		return order_obj;
+	}
+	
+	@Override
+	public List<Map<String, Object>> queryStoreCompleteInfo(String store_no){
+		String sql = "SELECT IFNULL(MAX(pro_total_gmv), 0) AS pro_total_gmv,IFNULL(MAX(ser_total_gmv), 0) AS ser_total_gmv,"
+				+ "IFNULL(MAX(gro_total_gmv), 0) AS gro_total_gmv,store_name,store_no FROM (SELECT CASE WHEN tor.order_tag2 = '1' "
+				+ "THEN	SUM(trading_price)	END AS pro_total_gmv,CASE WHEN tor.order_tag2 = '2' THEN SUM(trading_price) END AS ser_total_gmv,"
+				+ "CASE WHEN tor.order_tag2 = '3' THEN	SUM(trading_price) END AS gro_total_gmv,das.store_name,das.store_id,das.store_no,"
+				+ "tor.order_tag2 FROM	df_mass_order_monthly tor JOIN df_activity_scope das ON (tor.store_id = das.platformid) "
+				+ "WHERE TO_DAYS(NOW()) - TO_DAYS(tor.sign_time) <= 1 AND tor.order_tag2 IS NOT NULL ";
+		if(StringUtils.isNotEmpty(store_no)){
+			sql = sql + "AND tor.store_code='"+store_no+"'";
+		}
+		sql = sql +  "GROUP BY das.store_id,tor.order_tag2) aa GROUP BY store_id ORDER BY store_id";
 		
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
 		// 获得查询数据
@@ -96,9 +145,45 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 	}
 
 	@Override
-	public List<Map<String, Object>> queryDataOfPercent() {
-		String sql = "SELECT concat(tor.order_type,'') as order_type,sum(trading_price) AS ordergmv FROM df_mass_order_monthly tor JOIN df_activity_scope das on (tor.store_id = das.platformid) "
-				+ "WHERE tor.sign_time >= '2018-07-01' GROUP BY tor.order_type";
+	public List<Map<String, Object>> queryDataOfScatterplot() {
+		String sql = "SELECT das.store_name,tor.ordergmv,tor.ordernum FROM(SELECT tor.store_id,sum(trading_price) AS ordergmv,count(1) AS ordernum "
+				+ "FROM df_mass_order_monthly tor FORCE INDEX(sign_time) WHERE TO_DAYS(NOW()) - TO_DAYS(tor.sign_time) <= 1 AND tor.order_tag2 IS NOT NULL "
+				+ "GROUP BY tor.store_id) tor JOIN df_activity_scope das ON (tor.store_id = das.platformid)";
+		
+		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
+		// 获得查询数据
+		List<Map<String, Object>> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		return lst_data;
+	}
+
+	@Override
+	public List<Map<String, Object>> queryYestodayGmvRanking(String dept_id,String order_by){
+		String sql = "SELECT das.store_name,IFNULL(tor.ordergmv,0) AS ordergmv FROM (SELECT SUM(ordergmv) AS ordergmv,store_id FROM (SELECT tor.store_id,"
+				+ "sum(trading_price) AS ordergmv FROM df_mass_order_monthly tor WHERE TO_DAYS(NOW()) - TO_DAYS(tor.sign_time) <= 1 ";
+		if(StringUtils.isNotEmpty(dept_id)){
+			sql = sql + "AND tor.order_tag2 = '" + dept_id+"'";
+		}
+		sql = sql + "GROUP BY tor.store_id) aa GROUP BY	store_id) tor JOIN df_activity_scope das ON (tor.store_id = das.platformid)";			
+		if(StringUtils.isNotEmpty(order_by)){
+			sql = sql + "ORDER BY tor.ordergmv " + order_by;
+		}
+		sql = sql+" LIMIT 10";
+		
+		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
+		// 获得查询数据
+		List<Map<String, Object>> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		return lst_data;
+	}
+	
+	@Override
+	public List<Map<String, Object>> queryYestodayMemberRanking(String order_by){
+		String sql = "SELECT count(member.customer_id) AS membernum,das.store_name FROM	df_user_member member "
+				+ "JOIN df_activity_scope das ON (member.regist_storeid = das.platformid) WHERE	TO_DAYS(NOW()) - TO_DAYS(member.opencard_time) <= 1 "
+				+ "GROUP BY das.store_id ";
+		if(StringUtils.isNotEmpty(order_by)){
+			sql = sql + "ORDER BY membernum " + order_by;
+		}
+		sql = sql+" LIMIT 10";
 		
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
 		// 获得查询数据
@@ -109,16 +194,12 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 	@Override
 	public List<Map<String, Object>> queryStoreRanking(String dept_id){
 		String sql = "SELECT das.store_name,tor.ordergmv FROM (SELECT tor.store_id,sum(trading_price) AS ordergmv FROM df_mass_order_monthly tor "
-				+ "FORCE INDEX(sign_time) WHERE tor.sign_time >= '2018-07-01' ";
+				+ "FORCE INDEX(sign_time) WHERE tor.sign_time >= '2018-07-01' AND tor.order_tag2 IS NOT NULL ";
 		if(StringUtils.isNotEmpty(dept_id)){
-			if("groupon".equals(dept_id)){
-				sql  = sql + " AND tor.groupon_instance_id IS NOT NULL ";
-			} else {
-				sql  = sql + " AND tor.bussiness_group_id = '"+dept_id+"'";
-			}
+			sql  = sql + " AND tor.order_tag2='"+dept_id+"' ";
 		}
 		sql = sql + "GROUP BY tor.store_id) tor JOIN df_activity_scope das "
-				+ "ON (tor.store_id = das.platformid) ORDER BY tor.ordergmv desc limit 10";
+		+ "ON (tor.store_id = das.platformid) ORDER BY tor.ordergmv desc limit 10";
 		
 		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
 		// 获得查询数据
@@ -148,13 +229,14 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 	}
 	
 	@Override
-	public List<Map<String, Object>> queryProductRanking(String product_type,String store_no){
+	public List<Map<String, Object>> queryProductRanking(String dept_id,String store_no){
 		String sql = "SELECT tor.product_name, tor.product_quantity FROM (SELECT tor.product_name,  tor.store_id,sum(product_quantity) AS product_quantity "
-				+ "FROM ds_ope_product_store_day tor WHERE tor.recdate >= '2018-07-01' ";
-		
-		if(StringUtils.isNotEmpty(product_type)){
-			sql = sql + " AND tor.product_type = '"+product_type+"' ";
+				+ "FROM ds_ope_product_store_day tor JOIN df_activity_bussiness_scope dab ON (tor.channel_id = dab.id AND dab. LEVEL = 2";
+		if(StringUtils.isNotEmpty(dept_id)){
+			sql  = sql + " AND dab.type='"+dept_id+"' ";
 		}
+		sql = sql + ") WHERE tor.recdate >= '2018-07-01' ";
+		
 		if(StringUtils.isNotEmpty(store_no) && !"all".equals(store_no)){
 			sql = sql + " AND tor.store_no = '"+store_no+"' ";
 		}
@@ -164,5 +246,61 @@ public class StrategyActivityDaoImpl extends BaseDAOHibernate implements Strateg
 		// 获得查询数据
 		List<Map<String, Object>> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
 		return lst_data;
+	}
+	
+	@Override
+	public List<Map<String, Object>> queryTypeGMV() {
+		/**
+		 * @author wuxinxin
+		 * 2018年7月16日
+		 */
+		String sql = "select " + 
+				"	dateSeven.dates selDate, " + 
+				"	ifnull(datas.allcount, 0) cou, " + 
+				"	dateSeven.ordt as ordtypes " + 
+				"from " + 
+				"	( " + 
+				"		select distinct " + 
+				"			date(dmom0.sign_time) dates, " + 
+				"			alltype.tag2 ordt " + 
+				"		from " + 
+				"			df_mass_order_monthly dmom0, " + 
+				"			( " + 
+				"				select distinct " + 
+				"					dmom1.order_tag2 tag2 " + 
+				"				from " + 
+				"					df_mass_order_monthly dmom1 " + 
+				"				where " + 
+				"					dmom1.order_tag2 is not null " + 
+				"			) alltype " + 
+				"		where " + 
+				"			DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(dmom0.sign_time) " + 
+				"		and CURDATE() > date(dmom0.sign_time) " + 
+				"	) dateSeven " + 
+				"left join ( " + 
+				"	select " + 
+				"		TRUNCATE ((SUM(dmom.trading_price)) / 10000,2) AS allcount, " + 
+				"		DATE_FORMAT(dmom.sign_time, \"%Y-%m-%d\") as crtime, " + 
+				"		dmom.order_tag2 as ordtype " + 
+				"	from " + 
+				"		df_mass_order_monthly dmom, " + 
+				"		df_activity_scope das " + 
+				"	where " + 
+				"		dmom.store_id = das.platformid " + 
+				"	and DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(dmom.sign_time) " + 
+				"	and CURDATE() > date(dmom.sign_time) " + 
+				"	and dmom.order_tag2 is not null " + 
+				"	group by " + 
+				"		DATE_FORMAT(dmom.sign_time, \"%Y-%m-%d\"), " + 
+				"		dmom.order_tag2 " + 
+				") datas on (dateSeven.dates = datas.crtime and dateSeven.ordt=datas.ordtype)" + 
+				"order by " + 
+				"	dateSeven.dates, " + 
+				"	dateSeven.ordt";
+		Query query = this.getHibernateTemplate().getSessionFactory().getCurrentSession().createSQLQuery(sql);
+		// 获得查询数据
+		List<Map<String, Object>> lst_data = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		return lst_data;
+		
 	}
 }
--- src/com/cnpc/pms/festival/manager/StrategyActivityManager.java
+++ src/com/cnpc/pms/festival/manager/StrategyActivityManager.java
@@ -15,9 +15,15 @@ import com.cnpc.pms.base.manager.IManager;
 public interface StrategyActivityManager extends IManager{
 	
 	/**
-	 * 统计221GMV/优易GMV/家务事GMV
+	 * 统计221商品类/服务类/团购GMV
+	 * @param dept_id:1商品类，2服务类，3团购类
 	 */
-	public Map<String, Object> queryStrategyGMV(String dept_id,String start_time,String end_time);
+	public Map<String, Object> queryStrategyGMV(String dept_id,String store_no,String start_time,String end_time);
+	
+	/**
+	 * 统计非221GMV，按事业群分组
+	 */
+	public List<Map<String, Object>> queryUnStrategyGMV(String start_time,String end_time);
 	
 	/**
 	 * 查询GMV走势
@@ -27,12 +33,17 @@ public interface StrategyActivityManager extends IManager{
 	/**
 	 * 统计221新增社员数
 	 */
-	public Map<String, Object> queryNewMember();
+	public Map<String, Object> queryNewMember(String store_no,String start_time,String end_time);
 	
 	/**
 	 * 统计221总社员数
 	 */
 	public Map<String, Object> queryTotalMember();
+	
+	/**
+	 * 统计221单店完成情况
+	 */
+	public List<Map<String, Object>> queryStoreCompleteInfo(String store_no);
 
 	/**
 	 * 查询221门店GMV散点图
@@ -40,12 +51,20 @@ public interface StrategyActivityManager extends IManager{
 	public List<Map<String, Object>> queryDataOfScatterplot();
 	
 	/**
-	 * 查询商品类/服务类占比
+	 * 查询昨日门店GMV排名
+	 * @param dept_id:1商品类，2服务类，3团购类
+	 * @return
 	 */
-	public List<Map<String, Object>> queryDataOfPercent();
+	public List<Map<String, Object>> queryYestodayGmvRanking(String dept_id,String order_by);
 	
 	/**
-	 * 查询优易、家务事、团购门店排名
+	 * 查询昨日门店社员排名
+	 */
+	public List<Map<String, Object>> queryYestodayMemberRanking(String order_by);
+	
+	/**
+	 * 查询商品类/服务类/团购门店排名
+	 * @param dept_id:1商品类，2服务类，3团购类
 	 */
 	public List<Map<String, Object>> queryStoreRanking(String dept_id);
 	
@@ -61,6 +80,13 @@ public interface StrategyActivityManager extends IManager{
 	
 	/**
 	 * 查询商品类/服务类排名
+	 * @param dept_id:1商品类，2服务类，3团购类
 	 */
-	public List<Map<String, Object>> queryProductRanking(String product_type,String store_no);
+	public List<Map<String, Object>> queryProductRanking(String dept_id,String store_no);
+	
+	/**
+	 * 统计211分类GMV   1：商品    2.服务       3.团购
+	 * 
+	 */
+	public Map<String, Object> queryTypeGmv();
 }
