43d97d4571d70de6aea343aa7a6b0722e5d9cd28
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-07-31 16:04:47 +0800

    添加登录时在职判断的校验

--- src/com/cnpc/pms/bizbase/rbac/usermanage/manager/impl/UserManagerImpl.java
+++ src/com/cnpc/pms/bizbase/rbac/usermanage/manager/impl/UserManagerImpl.java
@@ -494,7 +494,7 @@ public class UserManagerImpl extends BizBaseCommonManager implements
 					.getSimpleFilter("code", userCode).appendOr(FilterFactory
 							.getSimpleFilter("phone", userCode)).appendOr(FilterFactory
 							.getSimpleFilter("mobilephone", userCode)).appendOr(FilterFactory
-							.getSimpleFilter("employeeId", userCode))).appendAnd(FilterFactory
+							.getSimpleFilter("employeeId", userCode))).appendAnd(FilterFactory.getSimpleFilter("disabledFlag=1")).appendAnd(FilterFactory
 					.getSimpleFilter("password", password))));
 		}
 		
