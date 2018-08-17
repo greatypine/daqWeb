7f1e2bda996173d4f98f7816df90506334fd9425
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-08-17 11:22:08 +0800

    新增外部人员修改功能，添加身份证 手机号等位数校验

--- src/com/cnpc/pms/personal/manager/ExternalHumanresourcesManager.java
+++ src/com/cnpc/pms/personal/manager/ExternalHumanresourcesManager.java
@@ -4,9 +4,16 @@ import java.io.File;
 import java.util.List;
 
 import com.cnpc.pms.base.manager.IManager;
+import com.cnpc.pms.personal.entity.ExternalHumanresources;
 
 public interface ExternalHumanresourcesManager extends IManager{
 	
 	
 	 public String saveHumanresourceExt(List<File> lst_import_excel) throws Exception ;
+	 
+	 
+	 public ExternalHumanresources queryExternalHumanresourcesById(Long id);
+	 
+	 
+	 public ExternalHumanresources updateExternalHumanresources(ExternalHumanresources externalHumanresources);
 }
