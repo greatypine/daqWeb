31d338b1c8a91c929a3a6ea6f1254935ed22df42
wuxinxin <wuxinxin@DESKTOP-ADVDMJ0>
2018-09-20 18:56:47 +0800

    修改页面标题

--- WebContent/crm/dist/js/headquarters/index_headquarters.js
+++ WebContent/crm/dist/js/headquarters/index_headquarters.js
@@ -4748,7 +4748,18 @@ function goToYouyiInfo(){
     }
     window.open(url,"youyi_product");
 }
-  
+    //社员简报  //全国
+    function goToMemberInfo(){
+        var role = curr_user.usergroup.code;
+        var url = "";
+        var target=pageStatusInfo.targets;
+        if(target==0){
+            url = "member_info.html?t="+encode64('0')+"&s=r="+encode64(role)+"&c=&cn=&e="+encode64(curr_user.id)+"&#fg";
+        }else if(target==1){
+            url = "member_info.html?t="+encode64(1)+"&s=&c="+ encode64(pageStatusInfo.cityId)+"&cn="+encode64(pageStatusInfo.cityName)+"&e="+encode64(curr_user.id)+"&#fg";
+        }
+        window.open(url,"member_info");
+    }
   //数据模型-社区模型-基础数据模型  //全国
   function toToBaseDataModule(){
   	  var url = "";
